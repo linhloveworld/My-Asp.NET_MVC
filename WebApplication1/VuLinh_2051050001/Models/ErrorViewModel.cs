0493532c9563dfd203ab9644bcee6564012bf6c6
Vu Linh <vu.linh1807@gmail.com>
2022-08-28 21:49:06 +0700

    homework of asp.net mvc
    bai tap ve nha

---
+++ WebApplication1/VuLinh_2051050001/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VuLinh_2051050001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
