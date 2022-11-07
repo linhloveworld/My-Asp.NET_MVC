9efcc8256fc4f24e70aef70699434ae3be3f2e6c
Vu_Linh_ThinkPadE14 <Vu_Linh_ThinkPadE14@THINKPADE14>
2022-11-07 22:48:04 +0700

    Commit_node

---
+++ VuLinh_BTH2_3-11/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+namespace VuLinh_BTH2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOption<ApplicationDbContext> option) : base(option)
+        { 
+            public DbSet<Student> Students { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+    }
+
+    public static implicit operator ApplicationDbContext(ApplicationDbContext v)
+    {
+        throw new NotImplementedException();
+    }
+}
+
